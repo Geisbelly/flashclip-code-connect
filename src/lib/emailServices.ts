0ff915c9eb343b16308cab764851ed45d0b319f2
Geisbelly <geisbelly19@gmail.com>
2025-05-05 10:07:42 -0300

    ups

--- src/lib/emailServices.ts
+++ src/lib/emailServices.ts
@@ -48,3 +48,25 @@ export async function sendContactEmail(data: {
       throw new Error("Erro ao enviar email.");
     }
   }
+
+  import sgMail from '@sendgrid/mail';
+
+  export async function email2Confirmacao(){
+    const chave = import.meta.env.VITE_EMAIL_API_CHAVE
+    sgMail.setApiKey(chave)
+  const msg = {
+    to: 'test@example.com', // Change to your recipient
+    from: 'test@example.com', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
