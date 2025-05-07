e706b6c15d62fea4eeb22c9294f38db25abf40fc
Geisbelly <geisbelly19@gmail.com>
2025-05-07 10:36:51 -0300

    Services planilhas

--- src/lib/googleServices.ts
+++ src/lib/googleServices.ts
@@ -3,10 +3,37 @@ const sheetId = '2PACX-1vQ3nvgTvC4ULYQsB6renOL34P_83EgECG6DwQjVRAnmRF7UUoCHyTsey
 const gid = '179122432';
 
 // URL de planilha publicada em formato CSV (mais fácil de parsear)
-const url = `https://docs.google.com/spreadsheets/d/e/${sheetId}/pub?gid=${gid}&single=true&output=csv`;
+  const url = `https://docs.google.com/spreadsheets/d/e/${sheetId}/pub?gid=${gid}&single=true&output=csv`;
 
   export default async function fetchSheetCSV() {
     const res = await fetch(url);
+    const text = await res.text();
+  
+    // Parse simples CSV → Array de arrays
+    const rows = text.trim().split('\n').map(row => row.split(','));
+    return rows;
+  }
+
+
+  
+  export async function getInstrutores() {
+    const Id = '16LKMRntSZlZyY5poRCHP-pm8vVwEeyz3dihIhDqgYKQ'
+    const Grid = '1427899878'
+    const link = `https://docs.google.com/spreadsheets/d/e/${Id}/pub?gid=${Grid}&single=true&output=csv`;
+    const res = await fetch(link);
+    const text = await res.text();
+  
+    // Parse simples CSV → Array de arrays
+    const rows = text.trim().split('\n').map(row => row.split(','));
+    return rows;
+  }
+
+
+  export async function getCursos() {
+    const Id = '16LKMRntSZlZyY5poRCHP-pm8vVwEeyz3dihIhDqgYKQ'
+    const Grid = '811359191'
+    const link = `https://docs.google.com/spreadsheets/d/e/${Id}/pub?gid=${Grid}&single=true&output=csv`;
+    const res = await fetch(link);
     const text = await res.text();
   
     // Parse simples CSV → Array de arrays
