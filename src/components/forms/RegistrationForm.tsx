4c416263df924c297c0f00f88c87cabc684f9a34
Geisbelly <geisbelly19@gmail.com>
2025-05-23 13:53:27 -0300

    fechar C#

--- src/components/forms/RegistrationForm.tsx
+++ src/components/forms/RegistrationForm.tsx
@@ -439,6 +439,9 @@ const RegistrationForm = () => {
           render={({ field }) => (
             <FormItem>
               <FormLabel className="text-white">Qual minicurso deseja participar? *</FormLabel>
+              <p className="text-xs text-white/60">
+                  Cursos com turmas lotadas: C#
+                </p>
               <Select onValueChange={field.onChange} defaultValue={field.value}>
                 <FormControl>
                   <SelectTrigger className="bg-tech-blue-light/30 border-white/10 text-white">
