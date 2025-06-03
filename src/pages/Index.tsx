417ba859bc2d902467bbfb3dd9c5b50acfacb112
Geisbelly <geisbelly19@gmail.com>
2025-06-03 14:02:25 -0300

    data e horario

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -47,7 +47,7 @@ const Index = () => {
             </div>
             
             <div className="mt-12 text-white/50 animate-fade-in" style={{ animationDelay: "0.8s" }}>
-              <p>Local: {eventInfo.location}</p>
+              <p>Local: {eventInfo.location} 07/06 às 14h</p>
             </div>
           </div>
         </div>
